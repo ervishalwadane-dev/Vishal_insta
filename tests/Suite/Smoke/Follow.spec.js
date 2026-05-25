7b0b5b84f6c81b224153c2fa49929ab1ea60e301
Vishal Wadane <er.vishalwadane@gmail.com>
2026-05-25 14:05:38 +0530

    add instgram Follow modification

--- tests/Suite/Smoke/Follow.spec.js
+++ tests/Suite/Smoke/Follow.spec.js
@@ -4,12 +4,21 @@ test('test', async ({ page }) => {
    await page.goto('https://www.instagram.com/');
   //await page.getByRole('textbox', { name: 'Mobile number, username or' }).click();
   await page.getByRole('textbox', { name: 'Mobile number, username or' }).fill('sushsingh2026');
-  //await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Mobile number, username or' }).press('Tab');
   await page.getByRole('textbox', { name: 'Password' }).fill('bug123');
+  await page.getByRole('textbox', { name: 'Password' }).press('Tab');
   await page.getByRole('button', { name: 'Log In', exact: true }).click();
-  await page.goto('https://www.instagram.com/accounts/onetap/');
   await page.getByRole('button', { name: 'Not now' }).click();
+  await page.goto('https://www.instagram.com/');
   await page.getByRole('button', { name: 'Not Now' }).click();
-  await page.getByRole('link', { name: 'jagt.apnagesh267', exact: true }).click();
-  await page.getByRole('button', { name: 'Follow' }).click();
+  await page.getByRole('link', { name: 'Messages' }).click();
+  await page.getByRole('button', { name: 'user-profile-picture' }).click();
+  await page.getByRole('textbox').nth(1).fill('How are you vishal');
+  await page.getByRole('button', { name: 'Send' }).click();   
+  await page.getByRole('button', { name: 'Add Photo or Video' }).click(); 
+  await page.getByRole('button', { name: 'Add Photo or Video' }).setInputFiles('Sample Post.jpg');
+  await page.getByRole('button', { name: 'Send' }).click();
+
+
 });
+
